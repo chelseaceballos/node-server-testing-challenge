46881cac207a516879973ada9e8c0dd2edcff273
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-19 12:42:13 -0700

    wip

--- api/friends/friends-model.js
+++ api/friends/friends-model.js
@@ -1,7 +1,28 @@
+const db = require('../../data/db-config')
+ 
+function getAll(){
+     return db('friends')
+}
+
  function getFriendById(friend_id){
-     return Promise.resolve(`friend id: ${friend_id} has been found`)
+    return db('friends').where('friend_id', friend_id).first()
  }
+
+ async function insert(friend) {
+    return db('friends')
+    .insert(friend)
+    .then(([friend_id]) => {
+      return getFriendById(friend_id)
+    })
+  }
+
+  function remove(friend_id) {
+    return null
+  }
 
  module.exports = {
      getFriendById,
+     getAll,
+     insert,
+     remove,
  }
--- api/friends/friends-router.js
+++ api/friends/friends-router.js
@@ -1,6 +1,16 @@
 const router = require("express").Router()
 const Friend = require('./friends-model')
 
+
+router.get('/', async (req, res, next) => {
+  try{
+    const friends = await Friend.getAll()
+    res.json(friends)
+  } catch (err) {
+      res.status(500).json(err)
+  }
+})
+
 router.get('/:friends_id', (req, res, next) => {
     Friend.getFriendById(req.params.friends_id)
         .then(found => {
@@ -9,7 +19,15 @@ router.get('/:friends_id', (req, res, next) => {
         .catch(next)
 })
 
+router.post('/', /*validateName,*/ async (req, res, next) => {
+    res.end()
+    //201 OK new friend
+})
 
+server.delete("/:friend_id", (req, res, next) => {
+    res.end()
+    // 200 OK
+  });
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
